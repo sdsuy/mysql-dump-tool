7ea39a556209913f9256dbab832a361179b605cc
Santiago Da Silva <santiagodasilva@sdsuy.dev>
2025-06-15 13:13:32 -0300

    Update MySQLConnector.java
    MySQLConnector

--- src/MySQLConnector.java
+++ src/MySQLConnector.java
@@ -1,3 +1,11 @@
 public class MySQLConnector {
-
+    private final String host;
+    private final String user;
+    private final String password;
+    
+    public MySQLConnector(String host, String user, String password) {
+        this.host = host;
+        this.user = user;
+        this.password = password;
+    }
 }
