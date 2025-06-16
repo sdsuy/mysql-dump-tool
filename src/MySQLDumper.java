b48444d19d2acb7ef5595439176b8a659a1c4473
Santiago Da Silva <santiagodasilva@sdsuy.dev>
2025-06-15 22:45:49 -0300

    Update MySQLDumper.java
    MySQLDumper

--- src/MySQLDumper.java
+++ src/MySQLDumper.java
@@ -1,3 +1,11 @@
 public class MySQLDumper {
+    private final String host;
+    private final String user;
+    private final String password;
 
+    public MySQLDumper(String host, String user, String password) {
+        this.host = host;
+        this.user = user;
+        this.password = password;
+    }
 }
